e1a6e069d3291e76842c55e754980627efe1b0cd
King0-0 <King0-0>
2024-03-19 15:44:37 +0900

    hi

---
+++ madang_b/src/main/java/com/example/demo/MadangBApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadangBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadangBApplication.class, args);
+	}
+
+}
